58d63ba7d6288ce6a737cad46351d9579a76b263
lennoj <jonnelross.mendoza@gmail.com>
2023-07-31 11:08:32 +1000

    Add files via upload
    Initial Commit - Additional Files (Test Files)

---
+++ TestPluginLoaderApp/TestPluginLoaderApp/TESTloader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VB62DotNetLoader.com.vb62dnl.Classes;
+
+
+namespace TESTloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter plugin name to test");
+            Console.Write("((e.g plugin.dll or \"plugin_test.dll\":");
+            string pluginPath = Console.ReadLine();
+
+            Console.Write("Enter the Plugin ID:");
+            string pluginId = Console.ReadLine();
+
+
+            PluginManager p = new PluginManager(null, "Testing");
+            p.LoadLibrary(pluginPath, pluginId);
+            p.Start(pluginId);
+        }
+    }
+}
